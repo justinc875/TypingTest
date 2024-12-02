e675ddd43de09a65bebdba85fdd36714a33c6c09
BT_1N3_12 <asdlasdf@gmail.com>
2024-12-02 09:57:09 -0500

    updated word list class

--- src/WordList.java
+++ src/WordList.java
@@ -1,43 +1,67 @@
 public class WordList {
-    //instance variables
+    // Instance variables
     private String wordList;
     private String wordTest;
-    private int comma;
 
-    //constructor
+    // Constructor
     public WordList() {
         wordList = "play,one,order,would,still,number,than,around,higher,less,know,less,power,through,number,could,we,take,against,stand,call,he,with,a,so,those,if,any,say,for,be,plan,each,give,which,face,first,increase,child,after,course,much,show,the,just,line,most,form,will,under,need,one,might,become,by,find,how,against,follow,all";
         wordTest = "";
-        comma = 0;
     }
 
-    //getter
+    // Getter for the word list
     public String getWordList() {
         return wordList;
     }
 
-    public void wordTest() {
-        //finds the amount of commas in the wordlist
-        for (int i = 0; i < wordList.length(); i++) {
-            if (wordList.substring(i, i+1).equals(",")) {
-                comma ++;
-            }
-        }
-        int index = (int) (Math.random() * comma) + 1;
-        String currentCharacter = wordList.substring(index + 1, index + 2);
-        int j = 0;
-        while (currentCharacter != ",") {
-            
+    // Generates a random list of 15 words
+    public void generateWordTest() {
+        wordTest = ""; // Reset wordTest for fresh generation
+        int wordCount = 0; // Tracks the number of words added
 
+        while (wordCount < 15) {
+            // Generate a random index within the length of the string
+            int randomIndex = (int) (Math.random() * wordList.length());
+
+            // Locate the word starting at the random index
+            int start = randomIndex;
+            int end = randomIndex;
+
+            // Move to the start of the word (preceding comma or start of string)
+            while (start > 0 && wordList.charAt(start - 1) != ',') {
+                start--;
+            }
+
+            // Move to the end of the word (next comma or end of string)
+            while (end < wordList.length() && wordList.charAt(end) != ',') {
+                end++;
+            }
+
+            // Extract the word
+            String word = wordList.substring(start, end);
+
+            // Append the word to the test string with a comma
+            wordTest += word + ",";
+            wordCount++;
+        }
+
+        // Remove the trailing comma
+        if (wordTest.endsWith(",")) {
+            wordTest = wordTest.substring(0, wordTest.length() - 1);
         }
     }
 
-    //math.random used to see which comma will be indexed out
-    //parse commas out using indexof
-    //list gets updated, word that gets parsed removed from list
-    //word added to wordtest
+    // Getter for the random word test
+    public String getWordTest() {
+        return wordTest;
+    }
 
-    /*if (wordList.substring(j, j + 1).equals(",")) {
-                count++;
-            }*/
+    // Main method to test the class
+    public static void main(String[] args) {
+        WordList wordList = new WordList();
+        wordList.generateWordTest();
+        System.out.println("Random Word List: " + wordList.getWordTest());
+    }
 }
+
+
