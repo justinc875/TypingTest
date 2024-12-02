23be1be4470cdd6dfddd1ae3bc442fa11a54f4ef
BT_1N3_11 <sample@bths.edu>
2024-12-02 10:43:37 -0500

    added logic to typetestlogic and wordlist
    wordlist is able to print and is working

--- src/TypeTestLogic.java
+++ src/TypeTestLogic.java
@@ -35,24 +35,35 @@ public class TypeTestLogic {
     public void started() {
         System.out.println("Type in \"A\" for a 15 word test or \"B\" for a 30 word test.");
         info = scan.nextLine();
-        while (!info.equals("A") || info.equals("B")) {
-            System.out.println("Please type in in the letter \"A\" for a 15 word test or the letter \"B\" for a 30 word test!");
+        while (!(info.equals("A") || info.equals("B"))) {
+            System.out.println("Please type in in the letter \"A\" for a 15 word test or the letter \"B\" for a 30 word test, please keep in mind the letters are case sensitive!");
             info = scan.nextLine();
         }
         mainMenuStart();
     }
 
-    //helper method
+    //helper methods
     private void mainMenuStart() {
         System.out.println("Input any character to start your test: ");
         start = scan.nextLine();
+        if (!(start.isEmpty())) { //intellij suggested to change this from .equals to .isEmpty
+            testObject();
+            System.out.println(wordTest);
+        }
     }
-
 
     public void printStats() {
         System.out.println("Good job!");
         System.out.println("Your wpm is ");
     }
 
+    private void testObject() {
+        WordList test = new WordList();
+        if (info.equals("A") || info.equals("B")) {
+            test.generateLoop();
+            test.generateWordTest();
+            wordTest = test.getWordTest();
+        }
+    }
 
 }
